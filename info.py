f8c6ce7192d85a872e2e7272629fcec4f6707314
Bookworm2024 <infohubnetworks@gmail.com>
2024-09-20 09:48:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -110,7 +110,7 @@ LINK_MODE = is_enabled("LINK_MODE", True)
 AUTO_FILTER = is_enabled('AUTO_FILTER', True)
 IMDB = is_enabled('IMDB', True)
 SPELL_CHECK = is_enabled("SPELL_CHECK", True)
-SHORTLINK = is_enabled('SHORTLINK', True)
+SHORTLINK = is_enabled('SHORTLINK', False)
 
 
 PAYMENT_QR = environ.get('PAYMENT_QR', 'https://envs.sh/wLE.jpg') #telegraph link of your QR code 
